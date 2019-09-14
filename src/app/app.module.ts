31c236a0b89cb575c8bf0795a2174f3913985c21
Mikhail Kiyan <Kiyan@ukr.net>
2019-09-14 17:46:54 +0300

    added Redux DevTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,9 +16,8 @@ import { postReducer } from './reducers/post.reducer';
     FormsModule,
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({
-      post: postReducer
-    })
+    StoreModule.forRoot({ post: postReducer }),
+    StoreDevtoolsModule.instrument({ maxAge: 10 /* number of states to retain */ })
   ],
   providers: [],
   bootstrap: [AppComponent]
