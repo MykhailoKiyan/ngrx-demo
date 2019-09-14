3145decf4ffea4dd7523695103ebb01f05b89307
Mikhail Kiyan <Kiyan@ukr.net>
2019-09-14 17:39:13 +0300

    Changed to full reducer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
+import { Post } from './models/post.model';
+import * as PostActions from './actions/post.actions';
+
+
 interface AppState {
-  message: string;
+  post: Post;
 }
 
 @Component({
@@ -11,18 +15,35 @@ interface AppState {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  message$: Observable<string>;
+export class AppComponent implements OnInit {
 
-  constructor(private store: Store<AppState>) {
-    this.message$ = this.store.select('message');
+  post$: Observable<Post>;
+
+  text: string; // from input val
+
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit(): void {
+    this.post$ = this.store.select('post');
   }
 
-  spanishMessage(): void {
-    this.store.dispatch({type: 'SPANISH'});
+  editText(): void {
+    const action = new PostActions.EditText(this.text);
+    this.store.dispatch(action);
   }
 
-  frenchMessage(): void {
-    this.store.dispatch({type: 'FRENCH'});
+  resetPost(): void {
+    const action = new PostActions.Reset();
+    this.store.dispatch(action);
+  }
+
+  upvote(): void {
+    const action = new PostActions.Upvote();
+    this.store.dispatch(action);
+  }
+
+  downvote(): void {
+    const action = new PostActions.Downvote();
+    this.store.dispatch(action);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { simpleReducer } from './simple.reducer';
+import { postReducer } from './reducers/post.reducer';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({ message: simpleReducer})
+    StoreModule.forRoot({
+      post: postReducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
